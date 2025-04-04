d2e67d44557f4f5f3ac09684cba0db5794ee0722
Mateuxx <motamatheus10@gmail.com>
2025-04-04 08:45:18 -0400

    Ui Deployment to s3

--- spacer-finder/src/infra/Launcher.ts
+++ spacer-finder/src/infra/Launcher.ts
@@ -2,22 +2,23 @@ import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { LambdaStack } from "./stacks/LamdaStack";
+import { UiDeploymentStack } from "./stacks/UiDeploymentStack";
 
 // Main CDK app configuration file that:
 // - Creates DynamoDB table (DataStack)
-// - Sets up Lambda functions (LambdaStack) 
+// - Sets up Lambda functions (LambdaStack)
 // - Configures API Gateway (ApiStack)
 // All stacks are connected to work together as a serverless API
 
-
 const app = new App();
 
-const dataStack = new DataStack (app, 'DataStack');
+const dataStack = new DataStack(app, "DataStack");
 const lambdaStack = new LambdaStack(app, "LambdaStack", {
-    spacesTable: dataStack.spacesTable
-}) 
+  spacesTable: dataStack.spacesTable,
+});
 
-new ApiStack  (app, 'ApiStack',{
-    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
-})
+new ApiStack(app, "ApiStack", {
+  spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
+});
 
+new UiDeploymentStack(app, "UiDeploymentStack");
---
+++ spacer-finder/src/infra/stacks/UiDeploymentStack.ts
@@ -0,0 +1,59 @@
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+import { existsSync } from "fs";
+import { join } from "path";
+
+export class UiDeploymentStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    //create the  bucket for storage the ui stuff
+    const deploymentBucket = new Bucket(this, "uiDeploymentBucket", {
+      bucketName: `space-finder-frontend-dev`.toLowerCase(),
+    });
+
+    // the dir of the ui folder
+    const uiDir = join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "space-finder-frontend",
+      "dist"
+    );
+    // ExistsSync verifies if a path exists
+    if (!existsSync(uiDir)) {
+      console.log("Ui dir not foun: " + uiDir);
+      return;
+    }
+
+    new BucketDeployment(this, "SpacesFinderDeployment", {
+      destinationBucket: deploymentBucket,
+      sources: [Source.asset(uiDir)], //ui directory
+    });
+
+    const originIdentity = new OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity"
+    );
+    deploymentBucket.grantRead(originIdentity);
+
+    const distribution = new Distribution(this, "SpacesFinderDistribution", {
+      defaultRootObject: "index.html",
+      defaultBehavior: {
+        origin: new S3Origin(deploymentBucket, {
+          originAccessIdentity: originIdentity,
+        }),
+      },
+    });
+
+    new CfnOutput(this, "SpaceFinderUrl", {
+      value: distribution.distributionDomainName,
+    });
+  }
+}
