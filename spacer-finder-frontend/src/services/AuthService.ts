050d0ecc74567652446a1cf4334549ac20404622
Mateuxx <motamatheus10@gmail.com>
2025-04-08 13:29:54 -0400

    Login Component

---
+++ spacer-finder-frontend/src/services/AuthService.ts
@@ -0,0 +1,16 @@
+export class AuthService {
+  //this is a async method to export a login
+  public async login(
+    username: string,
+    password: string
+  ): Promise<Object | undefined> {
+    return {
+      user: "abc",
+    };
+  }
+
+  //this a method to return the username
+  public getUserName() {
+    return "some user to return";
+  }
+}
