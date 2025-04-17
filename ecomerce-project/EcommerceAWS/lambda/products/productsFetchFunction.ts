33259e5c6b79be9b0f93720bfaf1a59bcac621c7
Mateuxx <motamatheus10@gmail.com>
2025-04-17 16:51:32 -0400

    Deploy da lambda

--- ecomerce-project/EcommerceAWS/lambda/products/productsFetchFunction.ts
+++ ecomerce-project/EcommerceAWS/lambda/products/productsFetchFunction.ts
@@ -9,16 +9,16 @@ export async function handler(
   context: Context
 ): Promise<APIGatewayProxyResult> {
   // id da funcção lambda e tals - identifica unicamente a requisição pela api gateway
-  const lambdaRequestId = context.awsRequestId;
-  const apiRequestID = event.requestContext.requestId;
+  // const lambdaRequestId = context.awsRequestId;
+  // const apiRequestID = event.requestContext.re;
 
   // see on the cloudwatch
   // gera custo na função lambda - cuidado com os logs - latencia e tals
   console.log(
-    `API Gateway Request ID ${apiRequestID} - Lambda Request ID: ${lambdaRequestId}`
+    // `API Gateway Request ID ${apiRequestID} - Lambda Request ID: ${lambdaRequestId}`
   );
   const mehtod = event.httpMethod;
-  if (event.resource === "/produtcs") {
+  if (event.resource === "/products") {
     if (mehtod === "GET") {
       console.log("GET");
     }
